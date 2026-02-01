df07c6b8912e6dc9fdfed3c82fadac4211e7f2a3
asuselessbrain <arfan18@cse.pstu.ac.bd>
2026-02-01 20:05:31 +0600

    fix: type related issue

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -11,20 +11,20 @@ const getAllAdminFromDB = catchAsync(async (req: Request, res: Response) => {
 
 const getSingleAdminFromDB = catchAsync(async (req: Request, res: Response) => {
     const id = req.params.id!;
-    const result = await AdminService.getSingleAdminFromDB(id);
+    const result = await AdminService.getSingleAdminFromDB(id as string);
     sendResponse(res, 200, "Admin retrieved successfully", result);
 })
 
 const updateAdminIntoDB = catchAsync(async (req: Request, res: Response) => {
     const adminId = req.params.id!;
     const updateData = req.body;
-    const result = await AdminService.updateAdminIntoDB(adminId, updateData);
+    const result = await AdminService.updateAdminIntoDB(adminId as string, updateData);
     sendResponse(res, 200, "Admin updated successfully", result);
 })
 
 const deleteAdminFromDB = catchAsync(async (req: Request, res: Response) => {
     const adminId = req.params.id!;
-    const result = await AdminService.deleteAdminFromDB(adminId);
+    const result = await AdminService.deleteAdminFromDB(adminId as string);
     sendResponse(res, 200, "Admin deleted successfully", result);
 })
 
